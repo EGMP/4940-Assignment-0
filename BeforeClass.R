3e445b40b6f9c583ea5f8fd66589dd3a10561c4b
Erin Menzies Pluer <46326698+EGMP@users.noreply.github.com>
2019-01-03 19:07:53 -0500

    Create BeforeClass.R

---
+++ BeforeClass.R
@@ -0,0 +1,28 @@
+#Hey there! This script will get you started getting comfortable in R
+#If you are looking at this script in GitHub you should download it and open it with RStudio on your computer.
+#To execute lines of code in the script hit CTRL+Enter or CTRL+R if you have an older version
+#Let's play around a bit
+  #Can you: 
+    #do some basic math?
+          12+3
+    #save a variable?
+          x=6 
+          y=5
+  	#What's: 
+        x+y 
+        z=x+y
+
+#Have you figured out the difference between the console and the script?
+        
+        
+#What about the environment? What's that? 
+        
+        
+#Create a new script and save it as "R Practice" in the folder you created for this class
+#Play around to figure out how to do this, explore the toolbar at the top
+
+#To write a comment in your script type #and then type. The text will turn green.    
+#Comment your answers to the above questions and play around a bit more in your "R Practice.R" script
+#You can also comment any questions you have at the bottom of your script
+#Submit your R Practice.R script as Assignment0 on GitHub classroom found here:
+# https://classroom.github.com/a/NvGfNMfH
